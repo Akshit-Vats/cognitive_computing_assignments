495b3f5ac6569cf7b0e69dacae8f8d85b9542943
Akshit Vats <142911909+Akshit-Vats@users.noreply.github.com>
2025-03-27 19:03:48 +0530

    Create Q4.py

---
+++ assignment-1/Q4.py
@@ -0,0 +1,18 @@
+#4.1
+for i in range(1,11):
+    print(i)
+
+print('\n')
+#4.2
+i=1
+while i<=10:
+    print(i)
+    i+=1
+
+print('\n')
+
+#4.3
+sum_for_loop=0
+for i in range(1,101):
+    sum_for_loop+=i
+print(f'The sum of numbers from 1 to 100 using a for loop is: {sum_for_loop}')
