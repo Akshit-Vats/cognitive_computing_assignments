14339ee23b8b88a83ee6c07dc16101d35fbe6554
Akshit Vats <142911909+Akshit-Vats@users.noreply.github.com>
2025-03-27 18:50:18 +0530

    Create Q2.py

---
+++ assignment-1/Q2.py
@@ -0,0 +1,23 @@
+#2.1
+a=10
+b=11
+print(a+b)
+s=a+b
+print(s)
+
+#2.2
+# Taking input from the user
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+# Concatenating the strings
+result = str1 + str2
+
+# Displaying the result
+print("Concatenated String:", result)
+
+#2.2
+text="AK"
+num=47
+result=text+str(num)
+print(result)
